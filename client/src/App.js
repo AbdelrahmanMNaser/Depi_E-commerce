1342f9ec8fcbc3caa37fb239038cf0ebf757f01a
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2024-10-23 17:39:35 +0300

    footer componets files

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Routing from "./Routing";
 import store from './redux/store';
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Router> 
       <Header />
       <h1 className="text-center text-green-600">DEPI E-Commerce</h1>
+      <Footer />
       <Routing />
       </Router> 
     </Provider>
