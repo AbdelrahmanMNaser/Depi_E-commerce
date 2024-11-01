0553ef9e282a70dc05b2b570b39f57a1271f2376
AbdelrahmanNasser77 <sim.abdelrahmanmohamed36283@alexu.edu.eg>
2024-11-01 21:24:48 +0200

    Refactor App component to integrate Redux provider and update product name to title in ProductCard

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductDetails from "./pages/productDetails";
+import { Provider } from "react-redux";
 import Routing from "./Routing";
 import store from "./redux/store";
 import { BrowserRouter as Router } from "react-router-dom";
